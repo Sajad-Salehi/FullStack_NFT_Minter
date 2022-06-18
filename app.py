d8d78a87667994069ee21dd4435ab44942789490
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-06-18 23:16:25 +0430

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from distutils.log import debug
+from pickle import TRUE
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -12,10 +14,13 @@ def connect_wallet():
 def home_page():
     return render_template("page_2.html")
 
-@app.route("/nftMinter")
+@app.route("/nftMinter", methods=["GET", "POST"])
 def nft_minter():
+    if request.method == "POST":
+        entry_content = request.form
+        print(entry_content)
     return render_template("page_3.html")
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=TRUE)
     
