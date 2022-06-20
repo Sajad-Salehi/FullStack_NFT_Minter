202c306a2c39f8b9dc19281b85baf09090b27e71
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-06-21 00:25:50 +0430

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from distutils.log import debug
 from pickle import TRUE
+import ipfshttpclient
+import json
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
-
 
 @app.route("/login")
 def connect_wallet():
@@ -19,18 +20,22 @@ def nft_minter():
     
     if request.method == "POST":
         title = request.form.get('content1')
-        artist = request.form.get('content2')
-        price = request.form.get('content3')
-        description = request.form.get('content4')
+        artistName = request.form.get('content2')
+        creator = request.form.get('content3')
+        price = request.form.get('content4')
+        description = request.form.get('content5')
+        img = request.files['file']
+                                
         NFT_info = {
             'Title': title,
-            'Artist': artist,
             'Price': price,
+            'Artist_name': artistName,
+            'Creator_addrs': creator,
             'description': description
         }
         print(NFT_info)
-        
     return render_template("page_3.html")
+
 
 if __name__ == '__main__':
     app.run(debug=TRUE)
