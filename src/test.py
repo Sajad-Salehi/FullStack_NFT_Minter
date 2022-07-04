a2169d4ba9adb41aae61b8279c0149af90421fcd
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-04 10:10:50 +0430

    Delete test.py

--- src/test.py
+++
@@ -1,5 +0,0 @@
-from importlib.metadata import metadata
-from mint_nft import mint_token
-
-metadata = "http://bafybeicbaie4svogxr5crz6tk6su6adf4o7ra2t54gu7eq3fepil5te7ie.ipfs.localhost:8080/"
-x = mint_token(metadata)
